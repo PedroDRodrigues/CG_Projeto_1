2fb7c9a303c984ad7455d89a34938ff6a0aca99e
Pedro Rodrigues <pedro.dias.rodrigues@tecnico.ulisboa.pt>
2023-05-17 18:31:58 +0100

    All parts of robot are added; Only missing translation and scalling of all except head and body

--- robo/js/robo.js
+++ robo/js/robo.js
@@ -6,28 +6,190 @@ var frontalCamera, lateralCamera, topCamera, perspectiveCamera, orthogonalCamera
 
 var material, geometry, mesh;
 
-function addTableTop(obj, x, y, z) {
+/*  CYLINDER CUBE SPHERE TORUS
+
+    HEAD  ---------> 2 ANTENNA                                      (1 CUBE) + (2 CYLINDERS) 
+          ---------> 2 EYES                                         (2 SPHERES)
+
+    ARMS  ---------> EACH ONE HAS ONE FOREARM AND AN EXHAUST PIPE   (2 CUBE) + 2 * (CUBE + CYLINDER)
+
+    BODY                                                            (1 CUBE)
+    
+    ABDOMEN                                                         (1 CUBE)
+
+    WAIST ---------> 2 ROADS                                        (1 CYLINDER) + (2 TORUS)   
+
+    THIGHS                                                          (2 CUBE)  
+
+    LEGS  ---------> EACH ONE HAS TWO ROADS                         (2 CUBE) + 2 * (2 TORUS)
+
+    FEETS                                                           (2 CUBE)
+
+    */
+
+function addHead(obj, x, y, z) {
     'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
+    geometry = new THREE.CubeGeometry(15, 12, 9);
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }); 
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.CylinderGeometry(1, 1, 5, 20);
+    material = new THREE.MeshBasicMaterial({ color: 0x7d3d3d, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x + 6, y + 8.5, z);
+    obj.add(mesh);
+
+    geometry = new THREE.CylinderGeometry(1, 1, 5, 20);
+    material = new THREE.MeshBasicMaterial({ color: 0x7d3d3d, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x - 6, y + 8.5, z);
+    obj.add(mesh);
+
+    /*geometry = new THREE.SphereGeometry(1, 5, 50);
+    material = new THREE.MeshBasicMaterial({ color: 0x008000, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x + 3, y + 1.5, z + 4.5);
+    obj.add(mesh);*/
+
+    geometry = new THREE.CircleGeometry(1, 5, 50);
+    material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x + 3, y + 1.5, z + 4.5);
+
+    geometry = new THREE.SphereGeometry(1, 5, 50);
+    material = new THREE.MeshBasicMaterial({ color: 0x008000, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x - 3, y + 1.5, z + 4.5);
+    obj.add(mesh);
+}
+
+function addArm(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(2, 10, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.CubeGeometry(2, 10, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z + 6);
+    obj.add(mesh);
+
+    geometry = new THREE.CylinderGeometry(1, 1, 2, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y - 5, z + 3);
+    obj.add(mesh);
+}
+
+function addBody(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(30, 30, 30);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x + 10, y - 21, z);
+    obj.add(mesh);
+}
+
+function addAbdomen(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(10, 10, 10);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
     obj.add(mesh);
 }
 
-function createTable(x, y, z) {
+function addWaist(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CylinderGeometry(2, 2, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.TorusGeometry(3, 1, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    //mesh.rotation.x = Math.PI / 2;
+    obj.add(mesh);
+
+    geometry = new THREE.TorusGeometry(3, 1, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    //mesh.rotation.x = Math.PI / 2;
+    //mesh.rotation.z = Math.PI / 2;
+    obj.add(mesh);
+}
+
+function addThigh(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(2, 10, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.CubeGeometry(2, 10, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z + 6);
+    obj.add(mesh);
+}
+
+function addLeg(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(2, 10, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.TorusGeometry(3, 1, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    //mesh.rotation.x = Math.PI / 2;
+    obj.add(mesh);
+
+    geometry = new THREE.TorusGeometry(3, 1, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    //mesh.rotation.x = Math.PI / 2;
+    //mesh.rotation.z = Math.PI / 2;
+    obj.add(mesh);
+}
+
+function addFeet(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(2, 2, 5);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+
+    geometry = new THREE.CubeGeometry(2, 2, 5);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z + 6);
+    obj.add(mesh);
+}
+
+function createRobo(x, y, z) {
     'use strict';
 
-    var table = new THREE.Object3D();
+    var robo = new THREE.Object3D();
 
     material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-    addTableTop(table, 0, 0, 0);
+    addHead(robo, 10, 0, 0);
+    addBody(robo, 0, 0, 0);
+    //addArm(robo, 10, 0, 0);
+    //addArm(robo, -10, 0, 0);
+    //addAbdomen(robo, -10, 0, 0);
+    //addWaist(robo, -20, 0, 0);
+    //addThigh(robo, -25, -5, 0);
+    //addLeg(robo, -25, -15, 0);
+    //addLeg(robo, -25, -20, 0);
+    //addFeet(robo, -25, -20, 0);
 
-    scene.add(table);
+    scene.add(robo);
 
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
+    robo.position.x = x;
+    robo.position.y = y;
+    robo.position.z = z;
 }
 
 function createScene() {
@@ -39,7 +201,7 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
 
-    createTable(0, 8, 0);
+    createRobo(0, 0, 0);
 }
 
 function createCamera() {
@@ -106,10 +268,10 @@ function init() {
     createCamera();
 
     //Camara frontal
-    frontalCamera = new THREE.OrthographicCamera(70, 
-            window.innerWidth / window.innerHeight,        
-            0.1,
-            1000);
+    frontalCamera = new THREE.OrthographicCamera(window.innerWidth / -10, 
+            window.innerWidth / 10,        
+            window.innerHeight / 10,
+            window.innerHeight / -10);
     frontalCamera.position.x = 0;
     frontalCamera.position.y = 0;
     frontalCamera.position.z = 50;
@@ -118,7 +280,7 @@ function init() {
     //Camara lateral
     lateralCamera = new THREE.OrthographicCamera(70,
             window.innerWidth / window.innerHeight,
-            0.1,
+            1,
             1000);
     lateralCamera.position.x = 50;
     lateralCamera.position.y = 0;
@@ -128,7 +290,7 @@ function init() {
     //Camara de topo
     topCamera = new THREE.OrthographicCamera(70,
             window.innerWidth / window.innerHeight,
-            0.1,
+            1,
             1000);
     topCamera.position.x = 0;
     topCamera.position.y = 50;
@@ -147,7 +309,8 @@ function init() {
     orthogonalCamera.position.x = 50;
     orthogonalCamera.position.y = 50;
     orthogonalCamera.position.z = 50;
-    orthogonalCamera.lookAt(scene.position);
+    var position = new THREE.Vector3(0, 0, 0);
+    orthogonalCamera.lookAt(position/*scene.position*/);
 
     render();
 
